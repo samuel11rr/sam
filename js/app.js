e12f769536a10a726934f8127ad2720a8542e131
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-03-30 18:40:41 -0600

    SECCION DE CONTADORES LISTA

--- js/app.js
+++ js/app.js
@@ -91,3 +91,114 @@ plxElement.style.height = altura+'px';
 let copyrightText = `Samuel Ramírez | CDMX 2016 - ${ new Date().getFullYear() }`;
 let copyrightElem = document.getElementById('copyright-text');
 copyrightElem.innerText = copyrightText;
+
+
+/******************************************
+ * FUNCION QUE AVANZA EL CARRUSEL
+ *****************************************/
+let carouselElem = document.getElementById('carousel-tech');
+
+function avanzaCarousel(){
+    setTimeout(() => {
+        let carousel = M.Carousel.getInstance(carouselElem);
+        carousel.next();
+        avanzaCarousel();
+    }, 5000);
+}
+// avanzaCarousel();
+
+
+
+/******************************************
+ * CONTADORES
+ *****************************************/
+// redimensionamos el div
+ let contadoresElem = document.getElementById('contadores');
+contadoresElem.style.height = '300px';
+
+// buscamos la posicion del div en el documento
+let contadoresX = contadoresElem.offsetTop;
+// console.log(contadoresX);
+
+let altoPantalla = window.innerHeight;
+let alturaDisparaA = contadoresX-(Math.round(altoPantalla/2));
+let alturaDisparaB = contadoresX+(Math.round(altoPantalla/2));
+// console.log(alturaDisparaA, alturaDisparaB);
+
+let aniosTotales = (new Date().getFullYear())-(new Date('2016').getFullYear()+1);
+let diasTrabajadosXanio = 20*12;
+
+// establecemos los datos
+let horasTrabajadas = 8*aniosTotales*diasTrabajadosXanio;
+let proyectosTerminados = aniosTotales*18;
+let lineasCodigo = proyectosTerminados*3500;
+let tazasCafe = diasTrabajadosXanio*aniosTotales*3;
+
+// determinamos si ha sido visible el div
+let contadoresVisible = false;
+
+// verificamos el scroll para disparar la funcion
+document.addEventListener('scroll', function(){
+    // console.log(window.pageYOffset);
+    
+    if( !contadoresVisible && (window.pageYOffset >= alturaDisparaA || window.pageYOffset <= alturaDisparaA) ){
+        // console.log('ES AQUI');
+        contadoresVisible = true;
+        activaContadores();
+    }
+});
+
+function activaContadores(){
+    contadorHoras();
+    contadorProyectos();
+    contadorLineas();
+    contadorTazas();
+}
+
+function contadorHoras( iteracion=0 ){
+    let horasElem = document.getElementById('horas-trabajo');
+    
+    iteracion+=50;
+    setTimeout(() => {
+        horasElem.innerText=iteracion.toLocaleString('es-MX');
+        if (iteracion < horasTrabajadas) {
+            contadorHoras(iteracion);
+        }
+    }, 5);
+}
+
+function contadorProyectos( iteracion=0 ){
+    let proyectosElem = document.getElementById('proyectos-terminados');
+    
+    iteracion+=2;
+    setTimeout(() => {
+        proyectosElem.innerText=iteracion.toLocaleString('es-MX');
+        if (iteracion < proyectosTerminados) {
+            contadorProyectos(iteracion);
+        }
+    }, 40);
+}
+
+function contadorLineas( iteracion=0 ){
+    let lineasElem = document.getElementById('lineas-codigo');
+    
+    iteracion+=2000;
+    setTimeout(() => {
+        lineasElem.innerText=iteracion.toLocaleString('es-MX');
+        if (iteracion < lineasCodigo) {
+            contadorLineas(iteracion);
+        }
+    }, 1);
+}
+
+function contadorTazas( iteracion=0 ){
+    let tazasElem = document.getElementById('tazas-cafe');
+    
+    iteracion+=20;
+    setTimeout(() => {
+        tazasElem.innerText=iteracion.toLocaleString('es-MX');
+        if (iteracion < tazasCafe) {
+            contadorTazas(iteracion);
+        }
+    }, 5);
+}
--- js/init.js
+++ js/init.js
@@ -1,20 +1,26 @@
 /*AQUI VAN LOS INICIADORES DE MATERIALIZE*/
 
-/* Iniciador de sidenav */
+/* En cuanto carga la pagina inicializamos los componentes Materialize */
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-        edge: 'right'
-    });
+  /* Iniciador de sidenav */
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {
+      edge: 'right'
   });
 
-/* Iniciador de parallax */
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {});
+  /* Iniciador de parallax */
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems, {});
+
+  /* Inicializador de Carrusel */
+  var carouselElem = document.querySelectorAll('.carousel');
+  var carouselInstance = M.Carousel.init(carouselElem, {
+    numVisible:5,
+    shift: 50,
+    dist: -40,
+    padding:50
+  });
 });
 
 /* Iniciador de particulas */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    // console.log('callback - particles.js config loaded');
-  });
+particlesJS.load('particles-js', 'assets/particles.json', function() {});
