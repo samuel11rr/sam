e242fdf75a6eda9b277c5628577604409fd59050
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-11-04 21:07:32 -0600

    ACTUALIZACION DE LINKS

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE    = 'static-v1-3';
+const STATIC_CACHE    = 'static-v1-4';
 const DYNAMIC_CACHE   = 'dynamic-v1-2';
 const INMUTABLE_CACHE = 'inmutable-v1-2';
 
